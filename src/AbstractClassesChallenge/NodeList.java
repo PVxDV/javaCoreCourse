1b8da408656017a36456c794c2fd59463f3da2ef
Pavel Rizhikh <rizhikh.pavel@yandex.ru>
2022-08-25 23:35:17 +0300

    load block 7-10

---
+++ src/AbstractClassesChallenge/NodeList.java
@@ -0,0 +1,9 @@
+package AbstractClassesChallenge;
+
+public interface NodeList {
+    ListItem getRoot();
+    boolean addItem(ListItem item);
+    boolean removeItem(ListItem item);
+    void traverse(ListItem root);
+}
+
