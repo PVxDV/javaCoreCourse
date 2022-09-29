0ae1de77feba6466543a37b1db7166c37fda2268
Pavel Rizhikh <rizhikh.pavel@yandex.ru>
2022-09-29 23:58:01 +0300

    Practice lectures 264

---
+++ src/Threads/Main.java
@@ -0,0 +1,9 @@
+package Threads;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from the main thread.");
+        Thread anotherThread = new AnotherThread();
+        anotherThread.start();
+    }
+}
