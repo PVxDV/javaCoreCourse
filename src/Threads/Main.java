faaf8acff4e0fd0780feffa07ece948c1cb10071
Pavel Rizhikh <rizhikh.pavel@yandex.ru>
2022-09-30 00:54:33 +0300

    Practice lectures 265

--- src/Threads/Main.java
+++ src/Threads/Main.java
@@ -1,9 +1,24 @@
 package Threads;
 
+import static Threads.ThreadColor.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from the main thread.");
+        System.out.println(ANSI_PURPLE + "Hello from the main thread.");
+
+        // create thread as named class
         Thread anotherThread = new AnotherThread();
         anotherThread.start();
+
+        // create thread as anonymous class. It has to be start immediately
+        new Thread() {
+            public void run() {
+                System.out.println(ANSI_CYAN + "Hello from anonymous class thread.");
+            }
+        }.start();
+
+
+
+        System.out.println(ANSI_PURPLE + "Hello again from the main thread");
     }
 }
